103eae0efe82756b85c6ffe146c3260927312931
Anonnymous Armadillo <pedro.argento@gmail.com>
2020-10-09 15:28:08 -0300

    first commit

---
+++ contracts/BinaryBet.sol
@@ -0,0 +1,97 @@
+pragma solidity ^0.6.8;
+
+contract BinaryBet {
+
+    BettingWindow firstWindow;
+    uint bettingWindowTotalSize;
+    uint blocksForBetting;
+    
+    enum BetSide {down, up} 
+    struct Bet {
+        uint betValue;
+        BetSide side;
+    }
+    
+    
+    struct Pool {
+        uint settlementBlock;
+        uint upValue;
+        uint downValue;
+        uint referencePrice;
+    }
+    
+    struct BettingWindow {
+        uint startingBlock;
+        Pool windowPool;
+    }
+    
+    mapping (uint => BettingWindow) windows;
+
+    
+    mapping(address => uint) balance;
+    
+
+    
+    constructor(uint _firstWindowBlock, uint _bettingWindowTotalSize, uint _blocksForBetting) public{
+        firstWindow = BettingWindow(_firstWindowBlock, Pool(_firstWindowBlock + _bettingWindowTotalSize, 0,0, getBlockPrice(block.number))) ;
+        blocksForBetting = _blocksForBetting;
+        bettingWindowTotalSize = _bettingWindowTotalSize;
+        windows[0] = firstWindow;
+    }
+
+    
+    function bet (uint value, BetSide side) payable external {
+        uint windowNumber = getBlockWindow(block.number);
+        uint startingBlock = getWindowStartingBlock(windowNumber);
+        uint lastBetBlock = getWindowLastBettingBlock(startingBlock);
+
+        require(block.number <= lastBetBlock, "bets closed for this window");
+        if(windows[windowNumber].windowPool.settlementBlock != 0) { //window exists (cant remember the best way to check this)
+            if (uint8(side) == 0) { //down
+              windows[windowNumber].windowPool.downValue += value;
+        
+            if (uint8(side) == 1) {
+              windows[windowNumber].windowPool.upValue += value;
+            }
+        }
+        }
+
+        else {
+            //create new pool 
+             if (uint8(side) == 0) { //down
+              Pool memory newPool = Pool(startingBlock + bettingWindowTotalSize, 0, value, getBlockPrice(startingBlock));
+              windows[windowNumber] = BettingWindow(startingBlock, newPool);
+             }
+             else  { //up
+              Pool memory newPool = Pool(startingBlock + bettingWindowTotalSize, value, 0, getBlockPrice(startingBlock));
+              windows[windowNumber] = BettingWindow(startingBlock, newPool);
+             }  
+        }
+    }        
+    
+        
+    
+
+    //Internal but set as public for testing
+    function getBlockWindow (uint currentBlock) public view returns (uint windowNumber) {
+        //n = floor((beg block - first_block)/window_size  + 1)
+        windowNumber = (currentBlock - windows[0].startingBlock)/bettingWindowTotalSize + 1; //integer division => floor    
+    }
+
+    //Internal but set as public for testing
+    function getWindowStartingBlock (uint windowNumber) public view returns (uint startingBlock) {
+        //firstBlock + (n-1)*window_size
+        startingBlock =  windows[0].startingBlock + (windowNumber -1)*bettingWindowTotalSize;
+    }
+    
+    //Internal but set as public for testing
+    function getWindowLastBettingBlock (uint startingBlock) public view returns (uint lastBettingBlock) {
+        return startingBlock + blocksForBetting;
+    }
+
+    //TODO Implement price API
+    function getBlockPrice(uint blockNumber) internal returns (uint currentPrice){
+        return 100;
+    }
+}
+
---
+++ test/window-find-test.js
@@ -0,0 +1,58 @@
+const { expect } = require("chai");
+
+describe("BinaryBets", function () {
+    it("Should find the correct starting block for the window", async function () {
+        const BinaryBet = await ethers.getContractFactory("BinaryBet");
+
+        let bet = await BinaryBet.deploy(10, 30, 2);
+        await bet.deployed();
+        expect(await bet.getWindowStartingBlock(12)).to.equal(340);
+        expect(await bet.getWindowStartingBlock(10)).to.equal(280);
+        expect(await bet.getWindowStartingBlock(21)).to.equal(610);
+
+        bet = await BinaryBet.deploy(5, 12, 4);
+        await bet.deployed();
+        expect(await bet.getWindowStartingBlock(12)).to.equal(137);
+        expect(await bet.getWindowStartingBlock(10)).to.equal(113);
+        expect(await bet.getWindowStartingBlock(35)).to.equal(413);
+
+
+
+    });
+
+    it("Should find the correct last bet block for the window", async function () {
+        const BinaryBet = await ethers.getContractFactory("BinaryBet");
+
+        let bet = await BinaryBet.deploy(10, 30, 2);
+        await bet.deployed();
+        expect(await bet.getWindowLastBettingBlock(340)).to.equal(342);
+        expect(await bet.getWindowLastBettingBlock(280)).to.equal(282);
+        expect(await bet.getWindowLastBettingBlock(610)).to.equal(612);
+
+        bet = await BinaryBet.deploy(5, 12, 4);
+        await bet.deployed();
+        expect(await bet.getWindowLastBettingBlock(137)).to.equal(141);
+        expect(await bet.getWindowLastBettingBlock(113)).to.equal(117);
+        expect(await bet.getWindowLastBettingBlock(413)).to.equal(417);
+
+    });
+
+    it("Should find the correct window number for block", async function () {
+        const BinaryBet = await ethers.getContractFactory("BinaryBet");
+
+        let bet = await BinaryBet.deploy(10, 30, 2);
+        await bet.deployed();
+        expect(await bet.getBlockWindow(168)).to.equal(6);
+        expect(await bet.getBlockWindow(330)).to.equal(11);
+        expect(await bet.getBlockWindow(749)).to.equal(25);
+
+        bet = await BinaryBet.deploy(5, 12, 4);
+        await bet.deployed();
+        expect(await bet.getBlockWindow(162)).to.equal(14);
+        expect(await bet.getBlockWindow(235)).to.equal(20);
+        expect(await bet.getBlockWindow(779)).to.equal(65);
+
+
+    });
+
+});
