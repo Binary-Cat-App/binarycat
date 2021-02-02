973ef734cb4d926b13bd6add646ed49dc4622587
jorka <joro.georgiev@gmail.com>
2021-02-02 10:51:08 +0200

    small fix

--- webapp/src/components/Alert.js
+++ webapp/src/components/Alert.js
@@ -4,11 +4,10 @@ export const Alert = ({ color = 'red', children = 'Alert Message' }) => {
   return (
     <>
       <div
-        className={
-          'text-white px-4 text-sm py-2 border-0 rounded relative mb-4 bg-' +
-          color +
-          '-500'
-        }
+        className={`text-white px-4 text-sm py-2 border-0 rounded relative mb-4 
+          ${color === 'red' && 'bg-red-500'}
+          ${color === 'green' && 'bg-green-500'}
+          ${color === 'yellow' && 'bg-yellow-500'}`}
       >
         {children}
       </div>
