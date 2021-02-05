851648ea35914dfedfcbcae8f1ed567482efccf7
Pedro Argento <pedro.argento@gmail.com>
2021-02-05 16:24:29 -0300

    deploys all contracts in chain

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 const BinBet = artifacts.require("BinaryBet");
+const BinToken = artifacts.require("BinToken");
+const BinStake = artifacts.require("BinaryStaking");
 
 module.exports = function (deployer) {
   deployer.deploy(BinBet, 10, 20, 5);
+  deployer.deploy(BinToken).then(function(){
+      return deployer.deploy(BinStake, BinToken.address)
+  });
 };
