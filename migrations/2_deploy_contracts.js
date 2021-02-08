5f6375156cfb5844eda45b1e6a1f62eb89b50509
Pedro Argento <pedro.argento@gmail.com>
2021-02-08 13:06:33 -0300

    fix deploy scritpt to set staking contract address in the bet contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,12 @@ const BinToken = artifacts.require("BinToken");
 const BinStake = artifacts.require("BinaryStaking");
 
 module.exports = function (deployer) {
-  deployer.deploy(BinBet, 10, 20, 5);
   deployer.deploy(BinToken).then(function(){
-      return deployer.deploy(BinStake, BinToken.address)
+      return deployer.deploy(BinStake, BinToken.address).then(function(){
+            deployer.deploy(BinBet, 10, 20, 5).then(function(){
+                BinBet.setStakingAddress(BinStake.address);
+            });
+
+      });
   });
 };
