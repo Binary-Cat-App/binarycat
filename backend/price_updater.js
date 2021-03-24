c68c2ebd8671355309912cc14d3c836e6beceafa
pedroargento <pedro.argento@gmail.com>
2021-03-24 15:23:54 -0300

    adds mongoDB that gets updated with band protocol prices while scripting is running (#33)

---
+++ backend/price_updater.js
@@ -0,0 +1,33 @@
+const { Client } = require('@bandprotocol/bandchain.js')
+
+// BandChain's Proof-of-Authority REST endpoint
+const endpoint = 'https://api-gm-lb.bandchain.org'
+const client = new Client(endpoint)
+
+PAIR = 'BNB/USD'
+INTERVAL = 10000 //milliseconds
+
+async function getPrice() {
+  const rate = await client.getReferenceData([
+    PAIR,
+  ])
+  return rate
+}
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+async function updateDB() {
+    let price = await getPrice()
+    var obj = {rate: price[0].rate, time: price[0].updatedAt.base}
+    MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("pricedb");
+    dbo.collection("BNBUSD").insertOne(obj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+    });
+});
+}
+
+var timer =  setInterval(updateDB, INTERVAL); // call every 1000 milliseconds
