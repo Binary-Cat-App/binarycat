72fd15348a39123144ec6c0853d88b5d1a7d0861
Deyan Makedonski <dmakedonski@hotmail.com>
2021-04-15 16:21:14 +0300

    Fix chart from - to period for better representation

--- webapp/src/components/Chart.js
+++ webapp/src/components/Chart.js
@@ -2,12 +2,37 @@ import React from 'react';
 import Chart from 'kaktana-react-lightweight-charts';
 
 export const BetChart = ({ classAlt, chart }) => {
+  // const [rest, setRest] = React.useState({});
+
+  const rest = React.useMemo(() => {
+    if (chart) {
+      if (chart.length > 0) {
+        // const start = new Date(chart[0].time * 1000);
+        // const end = new Date(chart[chart.length - 1].time * 1000);
+        // console.log({ from: start, to: end });
+        return { from: chart[0].time, to: chart[chart.length - 1].time };
+      }
+    }
+    return {};
+  }, [chart]);
+
+  // React.useEffect(() => {
+  //   if (chart) {
+  //     if (chart.length > 0) {
+  //       console.log(chart[chart.length - 1]);
+  //       const start = new Date(chart[0].time * 1000);
+  //       const end = new Date(chart[chart.length - 1] * 1000);
+  //       // console.log({ from: start, to: end });
+  //       setRest({ from: start, to: end });
+  //     }
+  //   }
+  // }, chart);
   const options = {
     layout: {
       backgroundColor: '#fff',
       textColor: 'rgba(33, 56, 77, 1)',
-      borderRadius: '20px',
-      overflow: 'hidden',
+      // borderRadius: '20px',
+      // overflow: 'hidden',
     },
     grid: {
       vertLines: {
@@ -35,7 +60,13 @@ export const BetChart = ({ classAlt, chart }) => {
     <div
       className={`bg-gray-100 rounded-3xl flex items-center justify-center text-white ${classAlt}`}
     >
-      <Chart options={options} lineSeries={lineSeries} autoWidth height={190} />
+      <Chart
+        options={options}
+        lineSeries={lineSeries}
+        autoWidth
+        height={190}
+        {...rest}
+      />
     </div>
   );
 };
