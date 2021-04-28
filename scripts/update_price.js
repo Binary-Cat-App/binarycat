bc082b72b89f742a0f5e06f8605b8c4528a88395
Pedro Argento <pedro.argento@gmail.com>
2021-04-28 17:19:55 -0300

    add script to call update price every x units of time"

--- scripts/update_price.js
+++ scripts/update_price.js
@@ -2,7 +2,22 @@ const Bin = artifacts.require('./BinaryBet.sol')
 const BN = require('bn.js');
 
 async function deployAll(contract) {
-    await contract.updatePrice()
+    const duration = await contract.windowDuration()
+    const firstBlock = await contract.firstBlock()
+    const firstWindow = await contract.firstWindow()
+    const offset = await contract.windowOffset()
+    let block = await web3.eth.getBlock("latest")
+    const blockNumber = block.number
+    const windowNumber = await contract.getWindowNumber(blockNumber, duration, firstBlock, offset, firstWindow);
+
+    const price = await contract.windowPrice(windowNumber)
+    if (price === 0) {
+        await contract.updatePrice()
+        console.log('price updated')
+    }
+    else {
+    }
+        console.log('already updated')
 }
 
 module.exports = function(done) {
