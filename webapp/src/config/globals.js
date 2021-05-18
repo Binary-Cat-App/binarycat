da2a8c7cd1b87349ded1f85da2bba5e81f49a470
Dimitar Angelkov <dimitar.angelkov@gmail.com>
2021-05-18 15:21:16 +0300

    Update globals.js

--- webapp/src/config/globals.js
+++ webapp/src/config/globals.js
@@ -1,5 +1,5 @@
 module.exports = global.config = {
   currencyName: 'BNB',
   currencyRequestValue: 'ether',
-  currencyRatesNodeAPI: (process.env.PORT) ? '' : 'http://localhost:5000'
+  currencyRatesNodeAPI: ''
 };
