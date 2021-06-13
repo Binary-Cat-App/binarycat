36396a22b53f961b7c73265fa4ade683c1c4ba27
Pedro Argento <pedro.argento@gmail.com>
2021-06-13 15:44:09 -0300

    adds scripts that bet both sides

---
+++ scripts/bet_sides.js
@@ -0,0 +1,31 @@
+const Bin = artifacts.require('./BinaryBet.sol')
+const BN = require('bn.js');
+
+async function deployAll(contract) {
+    let duration = await contract.windowDuration()
+    let firstBlock = await contract.firstBlock()
+    let firstWindow = await contract.firstWindow()
+    let offset = await contract.windowOffset()
+    let block = await web3.eth.getBlock("latest")
+    let blockNumber = block.number
+    let windowNumber = await contract.getWindowNumber(blockNumber, duration, firstBlock, offset, firstWindow);
+
+    let val = "100000000000000000"
+
+    let price = await contract.windowPrice(windowNumber)
+    if (price == 0) {
+        await contract.placeBet(val, 0, {value: val})
+        await contract.placeBet(val, 1, {value: val})
+        console.log('bets placed')
+    }
+    else {
+        console.log('already updated')
+    }
+}
+
+module.exports = function(done) {
+    Bin.deployed().then(function(contract) {
+        return deployAll(contract)
+    })
+    .then(()=> {console.log("Done!"); return done()}, (reason) => {console.log("Error:", reason); return done()});
+};
