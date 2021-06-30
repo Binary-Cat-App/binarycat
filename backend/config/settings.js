de819ba4814574bc7c0f97cfe9deccf46379874a
Dimitar Angelkov <dimitar.angelkov@gmail.com>
2021-06-30 11:01:01 +0300

    Updated Price data feed

--- backend/config/settings.js
+++ backend/config/settings.js
@@ -1,6 +1,7 @@
 module.exports = {
   port: process.env.PORT || 5000,
   db: process.env.MONGODB_URI,
-  PAIR: "BNB/USD",
+  CURRENCY_FEED_URL: process.env.CURRENCY_FEED_URL,
+  CURRENCY_FEED_CONTRACT_ADDRESS: process.env.CURRENCY_FEED_CONTRACT_ADDRESS,
   INTERVAL: process.env.CURRENCY_FEED_INTERVAL //milliseconds
 };
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const settings = require("./config/settings");
-const { Client } = require("@bandprotocol/bandchain.js");
+const Web3 = require("web3");
 const socketIo = require("socket.io");
 
-// BandChain's Proof-of-Authority REST endpoint
-const endpoint = "https://api-gm-lb.bandchain.org";
-const client = new Client(endpoint);
+const web3 = new Web3( settings.CURRENCY_FEED_URL );
+const aggregatorV3InterfaceABI = [{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
+const priceFeed = new web3.eth.Contract(aggregatorV3InterfaceABI, settings.CURRENCY_FEED_CONTRACT_ADDRESS);
 
 const io = socketIo(http, {
   cors: {
@@ -29,20 +29,26 @@ io.on("connect", socket => {
   socket.on("client-message", ({ action, payload }) => {});
 });
 
-async function getPrice() {
-  try {
-    const rate = await client.getReferenceData([settings.PAIR]);
-    return rate;
+function getPrice() {
+  try {    
+    priceFeed.methods.latestRoundData().call()
+      .then(( roundData ) => {
+        updateDB( roundData );
+      });
   } catch (e) {
     console.log(e.message)
     return null
   }
 }
 
-async function updateDB() {
-  let price = await getPrice();
-  if (price === null) return;
-  var obj = { rate: price[0].rate, time: price[0].updatedAt.base };
+function updateDB( data ) {
+  if (data === null || typeof data === 'undefined') return;
+
+  const price = data.answer / 100000000;
+  const time = data.startedAt;
+  
+  var obj = { rate: price.toFixed(4), time: time };
+
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("pricedb");
@@ -58,7 +64,7 @@ async function updateDB() {
 }
 
 if ( process.env.UPDATE_DB !== 'off' )
-  var timer = setInterval(updateDB, settings.INTERVAL);
+  var timer = setInterval(getPrice, settings.INTERVAL);
 
 http.listen(settings.port, () =>
   console.log(`Server listening on port ${settings.port}...`)
