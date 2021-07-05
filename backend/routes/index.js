6de14f991b9e4d2c6c6a781bdb5b5a702edf184b
Dimitar Angelkov <dimitar.angelkov@gmail.com>
2021-07-05 18:11:05 +0300

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -25,7 +25,6 @@ module.exports = app => {
           .limit(settings.currencyResultLimit)
           .toArray(function (err, result) {
               if (err) throw err;
-              console.log(result);
               db.close();
               res.json({ success: true, result });
           });
