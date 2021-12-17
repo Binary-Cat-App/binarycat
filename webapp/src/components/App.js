88fe7e5aa12b8b49064cdab61e29306080d23c66
Dimitar Angelkov <dimitar.angelkov@gmail.com>
2021-12-17 09:38:56 +0200

    Staking Button & Add KITTY token to MetaMask

--- webapp/src/components/App.js
+++ webapp/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useWeb3React } from "@web3-react/core";
+import { useWeb3React } from '@web3-react/core';
 
 import { Header } from './Header';
 import { Faq } from './Faq';
@@ -15,10 +15,9 @@ import { BettingProvider } from '../context/BettingContext';
 import { StakingProvider } from '../context/StakingContext';
 
 const RoutesComponent = () => {
-  
   const { active } = useWeb3React();
 
-  if ( !active )
+  if (!active)
     return (
       <div className="flex flex-col">
         <Router>
@@ -49,7 +48,7 @@ const RoutesComponent = () => {
   return (
     <div className="flex flex-col">
       <Router>
-        <Header />
+        <Header connected="true" />
         <div className="pt-6 sm:pt-12 flex-grow flex flex-col">
           <Container>
             <Switch>
@@ -81,9 +80,7 @@ const RoutesComponent = () => {
 const ConnectedRoutesComponent = RoutesComponent;
 
 export const App = (props) => {
-  return (
-    <ConnectedRoutesComponent />
-  );
+  return <ConnectedRoutesComponent />;
 };
 
 export default App;
--- webapp/src/components/Header.js
+++ webapp/src/components/Header.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Container } from './Container';
 import { ReactComponent as Logo } from '../assets/images/logo.svg';
 import { ReactComponent as IconHelp } from '../assets/images/icon-help.svg';
+import { ReactComponent as IconAdd } from '../assets/images/icon-add.svg';
 
-export const Header = () => {
-
+export const Header = ({ connected = false }) => {
   const location = useLocation();
-  const stakingBtn = location.pathname === '/staking' ? 'btn--green' : 'btn--outline';
+  const { active, account } = useWeb3React();
+  const ethereum = window.ethereum;
+
+  const [success, setSuccess] = useState(false);
+
+  const token = {
+    address: '0x7c0a374De8893283CE2010D322DbB9c10b23D8f8',
+    symbol: 'KITTY',
+    decimals: 18,
+  };
+
+  const handleAddToken = async () => {
+    if (active && account) {
+      ethereum
+        .request({
+          method: 'wallet_watchAsset',
+          params: {
+            type: 'ERC20',
+            options: {
+              address: token.address,
+              symbol: token.symbol,
+              decimals: token.decimals,
+            },
+          },
+        })
+        .then((success) => {
+          setSuccess(success);
+        })
+        .catch(() => setSuccess(false));
+    }
+  };
 
   return (
     <div className="pt-4 lg:pt-8 flex-shrink-0">
@@ -21,6 +52,18 @@ export const Header = () => {
           </span>
           <div>
             <ul className="flex items-center text-sm">
+              {connected && (
+                <li className="mr-4">
+                  <Link
+                    to=""
+                    onClick={handleAddToken}
+                    className="text-gray-500 hover:text-gray-800 flex items-center"
+                  >
+                    <IconAdd className="icon mr-2" />
+                    Add KITTY token to MetaMask
+                  </Link>
+                </li>
+              )}
               <li className="mr-4">
                 <Link
                   to="/help"
@@ -31,11 +74,17 @@ export const Header = () => {
                 </Link>
               </li>
               <li>
-                <Link 
-                  to="/staking"
-                  className={`btn ${stakingBtn}`}>
-                  Staking
-                </Link>
+                {location.pathname === '/staking' && (
+                  <Link to="/" className={`btn btn--outline`}>
+                    Bet
+                  </Link>
+                )}
+
+                {location.pathname === '/' && (
+                  <Link to="/staking" className={`btn btn--outline`}>
+                    Staking
+                  </Link>
+                )}
               </li>
             </ul>
           </div>
