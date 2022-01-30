322b29542a57a2b33e5d2bd40cb7f3573d6a1757
Dimitar Angelkov <dimitar.angelkov@gmail.com>
2022-01-30 17:18:48 +0200

    Staking Allowance mods

--- webapp/src/components/Stake.js
+++ webapp/src/components/Stake.js
@@ -5,152 +5,141 @@ import NumberFormat from 'react-number-format';
 import { useStaking } from '../context/StakingContext';
 
 export const Stake = () => {
-	const [value, setValue] = React.useState(0);
-	const [error, setError] = React.useState(false);
+  const [value, setValue] = React.useState(0);
+  const [error, setError] = React.useState(false);
 
-	const { 
-		active,
-    	account,
-		userAllowance,
-		walletBalance,
-		weiToCurrency,
-		currencyToWei,
-		stakingObj,
-		staking,
-    	tokenObj
-	} = useStaking();
-	
-	const _walletBalance = walletBalance ? weiToCurrency( walletBalance ) : 0.00;
-	const _userAllowance = userAllowance ? weiToCurrency( userAllowance ) : 0.00;
+  const {
+    active,
+    account,
+    userAllowance,
+    walletBalance,
+    weiToCurrency,
+    currencyToWei,
+    stakingObj,
+    staking,
+    tokenObj,
+  } = useStaking();
 
-	const handleAllowance = async (val) => {
-		
-		if ( active && account && _walletBalance > 0 && val > 0 && val <= _walletBalance ) {
+  const _walletBalance = walletBalance ? weiToCurrency(walletBalance) : 0.0;
+  const _userAllowance = userAllowance ? weiToCurrency(userAllowance) : 0.0;
 
-			const amount = currencyToWei(val + _userAllowance);
+  const allowanceThreshold = 1000000;
 
-			const allowance = await tokenObj.methods
-				.approve(staking.address, amount)
-				.send({
-					from: account
-				});
+  const handleAllowance = async (val) => {
+    if (active && account) {
+      const amount = currencyToWei(Number.MAX_SAFE_INTEGER);
 
-			if ( allowance ) {
-				setValue(0);
-				setError(false);
-			}
-		}
+      const allowance = await tokenObj.methods
+        .approve(staking.address, amount)
+        .send({
+          from: account,
+        });
+    }
+  };
 
-	};
+  const handleStake = async (val) => {
+    if (
+      active &&
+      account &&
+      _userAllowance > 0 &&
+      val > 0 &&
+      val <= _userAllowance
+    ) {
+      const amount = currencyToWei(val);
 
-	const handleStake = async (val) => {
-		
-		if ( active && account && _userAllowance > 0 && val > 0 && val <= _userAllowance ) {
+      const stake = await stakingObj.methods.stake(amount).send({
+        from: account,
+      });
 
-			const amount = currencyToWei(val);
+      if (stake) {
+        setValue(0);
+        setError(false);
+      }
+    }
+  };
 
-			const stake = await stakingObj.methods
-				.stake( amount )
-				.send({
-					from: account
-				});
+  return (
+    <>
+      <div className="mb-4">
+        <h2 className="text-center text-3xl font-medium">Stake</h2>
+      </div>
 
-			if ( stake ) {
-				setValue(0);
-				setError(false);
-			}
-		}
+      {error && (
+        <Alert color="red">
+          <span>{error}</span>
+        </Alert>
+      )}
 
-	};
+      {_userAllowance < Number.MAX_SAFE_INTEGER - allowanceThreshold && (
+        <div className="my-2">
+          <Button
+            variant="green"
+            size="normal"
+            className="w-full py-3 text-xl mb-8"
+            handleClick={() => {
+              handleAllowance(value);
+            }}
+          >
+            Top up your staking allowance
+          </Button>
+        </div>
+      )}
 
-	return (
-		<>
-			<div className="mb-4">
-				<h2 className="text-center text-3xl font-medium">Stake</h2>
-				<p className="text-sm text-gray-300 text-center">Allowance: {_userAllowance.toFixed(2)} {global.config.tokenName}</p>
-			</div>
+      {_userAllowance !== 0 && (
+        <>
+          <div className="min-w-0 flex items-center mb-8 py-2 px-4 bg-gray-100 rounded">
+            <Button
+              variant="gray"
+              outline
+              className="text-gray-800 font-normal py-1 px-2 border border-gray-400 rounded shadow"
+              handleClick={() => {
+                setValue(_walletBalance);
+              }}
+              isDisabled={_walletBalance === 0}
+            >
+              max
+            </Button>
 
-			{error && (
-            	<Alert color="red">
-              		<span>{error}</span>
-            	</Alert>
-          	)}
+            <NumberFormat
+              thousandSeparator=" "
+              decimalSeparator="."
+              decimalScale="2"
+              fixedDecimalScale="2"
+              allowNegative="false"
+              autoFocus
+              className="form-control font-digits text-xl border-0 bg-transparent text-center"
+              value={value}
+              onValueChange={(values) => {
+                const val = values.formattedValue.replace(/\s/gi, '');
+                setValue(Number(val));
+                setError(false);
+              }}
+            />
 
-			<div className="min-w-0 flex items-center mb-8 py-2 px-4 bg-gray-100 rounded">
-				<Button 
-					variant="gray"
-					outline
-					className="text-gray-800 font-normal py-1 px-2 border border-gray-400 rounded shadow"
-					handleClick={() => {
-						if (_userAllowance !== 0) {
-							setValue(_userAllowance);
-						} else {
-							setValue(_walletBalance);
-						}
-					}}
-					isDisabled={_walletBalance === 0}
-					>
-					max
-				</Button>
+            <label htmlFor="stakeAmount" className="ml-2 text-lg flex-shrink-0">
+              {global.config.tokenName}
+            </label>
+          </div>
 
-	            <NumberFormat
-					thousandSeparator=" "
-					decimalSeparator="."
-					decimalScale="2"
-					fixedDecimalScale="2"
-					allowNegative="false"
-					autoFocus
-					className="form-control font-digits text-xl border-0 bg-transparent text-center"
-					value={value}
-					onValueChange={(values) => {
-						const val = values.formattedValue.replace(/\s/gi, '');
-						setValue(Number(val));
-						setError(false);
-					}}
-	            />
-
-	            <label htmlFor="stakeAmount" className="ml-2 text-lg flex-shrink-0">
-					{global.config.tokenName}
-	            </label>
-			</div>
-
-			<div className="my-2">
-				<Button
-					variant="gray"
-					size="normal"
-					className="w-full py-3 text-xl"
-					outline
-					handleClick={() => {
-						if ( value > _walletBalance ) {
-							setError('Insufficient wallet balance.');
-						} else {
-							handleAllowance(value);
-						}
-					}}
-					isDisabled={_walletBalance === 0 || value === 0}
-				>
-					Approve
-				</Button>
-            </div>
-
-            <div className="my-2">
-				<Button
-					variant="green"
-					size="normal"
-					className="w-full py-3 text-xl"
-					handleClick={() => {
-						if ( value > _userAllowance ) {
-							setError('Insufficient allowance.');
-						} else {
-							handleStake(value);
-						}
-					}}
-					isDisabled={_userAllowance === 0 || value === 0}
-				>
-					Stake
-				</Button>
-            </div>
-		</>
-	);
-
+          <div className="my-2">
+            <Button
+              variant="green"
+              size="normal"
+              className="w-full py-3 text-xl"
+              handleClick={() => {
+                if (value > _userAllowance) {
+                  setError('Insufficient allowance.');
+                } else {
+                  handleStake(value);
+                }
+              }}
+              isDisabled={_userAllowance === 0 || value === 0}
+            >
+              Stake
+            </Button>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
--- webapp/src/components/Unstake.js
+++ webapp/src/components/Unstake.js
@@ -5,106 +5,105 @@ import NumberFormat from 'react-number-format';
 import { useStaking } from '../context/StakingContext';
 
 export const Unstake = () => {
-	const [value, setValue] = React.useState(0);
-	const [error, setError] = React.useState(false);
+  const [value, setValue] = React.useState(0);
+  const [error, setError] = React.useState(false);
 
-	const { 
-		active,
-    	account,
-		stakedBalance,
-		weiToCurrency,
-		currencyToWei,
-		stakingObj,
-    	tokenObj
-	} = useStaking();
+  const {
+    active,
+    account,
+    stakedBalance,
+    weiToCurrency,
+    currencyToWei,
+    stakingObj,
+    tokenObj,
+  } = useStaking();
 
-	const _stakedBalance = stakedBalance ? weiToCurrency(stakedBalance) : 0.00;
+  const _stakedBalance = stakedBalance ? weiToCurrency(stakedBalance) : 0.0;
 
-	const handleUnstake = async (val) => {
-		
-		if ( active && account && _stakedBalance > 0 && val > 0 && val <= _stakedBalance ) {
+  const handleUnstake = async (val) => {
+    if (
+      active &&
+      account &&
+      _stakedBalance > 0 &&
+      val > 0 &&
+      val <= _stakedBalance
+    ) {
+      const amount = currencyToWei(val);
 
-			const amount = currencyToWei(val);
+      const unstake = await stakingObj.methods.unstake(amount).send({
+        from: account,
+      });
 
-			const unstake = await stakingObj.methods
-				.unstake(amount)
-				.send({
-					from: account
-				});
-			
-			if (unstake) {
-				setValue(0);
-				setError(false);
-			}
-		}
+      if (unstake) {
+        setValue(0);
+        setError(false);
+      }
+    }
+  };
 
-	};
+  return (
+    <>
+      <div className="mb-4">
+        <h2 className="text-center text-3xl font-medium">Unstake</h2>
+      </div>
 
-	return (
-		<>
-			<div className="mb-4">
-				<h2 className="text-center text-3xl font-medium">Unstake</h2>
-				<p className="text-sm text-gray-300 text-center">&nbsp;</p>
-			</div>
+      {error && (
+        <Alert color="red">
+          <span>Insufficient staked balance!</span>
+        </Alert>
+      )}
 
-			{error && (
-            	<Alert color="red">
-              		<span>Insufficient staked balance!</span>
-            	</Alert>
-          	)}
+      <div className="min-w-0 flex items-center mb-8 py-2 px-4 bg-gray-100 rounded">
+        <Button
+          variant="gray"
+          outline
+          className="text-gray-800 font-normal py-1 px-2 border border-gray-400 rounded shadow"
+          handleClick={() => {
+            setValue(_stakedBalance);
+          }}
+          isDisabled={_stakedBalance === 0}
+        >
+          max
+        </Button>
 
-			<div className="min-w-0 flex items-center mb-8 py-2 px-4 bg-gray-100 rounded">
-				<Button 
-					variant="gray"
-					outline
-					className="text-gray-800 font-normal py-1 px-2 border border-gray-400 rounded shadow"
-					handleClick={() => {
-						setValue(_stakedBalance);
-					}}
-					isDisabled={_stakedBalance === 0}
-				>
-				max
-				</Button>
+        <NumberFormat
+          thousandSeparator=" "
+          decimalSeparator="."
+          decimalScale="2"
+          fixedDecimalScale="2"
+          allowNegative="false"
+          autoFocus
+          className="form-control font-digits text-xl border-0 bg-transparent text-center"
+          value={value}
+          onValueChange={(values) => {
+            const val = values.formattedValue.replace(/\s/gi, '');
+            setValue(Number(val));
+            setError(false);
+          }}
+        />
 
-	            <NumberFormat
-	              thousandSeparator=" "
-	              decimalSeparator="."
-	              decimalScale="2"
-	              fixedDecimalScale="2"
-	              allowNegative="false"
-	              autoFocus
-	              className="form-control font-digits text-xl border-0 bg-transparent text-center"
-	              value={value}
-	              onValueChange={(values) => {
-					const val = values.formattedValue.replace(/\s/gi, '');
-	                setValue(Number(val));
-					setError(false);
-	              }}
-	            />
+        <label htmlFor="stakeAmount" className="ml-2 text-lg flex-shrink-0">
+          {global.config.tokenName}
+        </label>
+      </div>
 
-	            <label htmlFor="stakeAmount" className="ml-2 text-lg flex-shrink-0">
-					{global.config.tokenName}
-	            </label>
-			</div>
-
-            <div className="my-2">
-				<Button
-					variant="blue"
-					size="normal"
-					className="w-full py-3 text-xl"
-					handleClick={() => {
-						if ( value > _stakedBalance ) {
-							setError(true);
-						} else {
-							handleUnstake(value);
-						}
-					}}
-					isDisabled={_stakedBalance === 0 || value === 0}
-				>
-					Unstake
-				</Button>
-            </div>
-		</>
-	);
-
+      <div className="my-2">
+        <Button
+          variant="green"
+          size="normal"
+          className="w-full py-3 text-xl"
+          handleClick={() => {
+            if (value > _stakedBalance) {
+              setError(true);
+            } else {
+              handleUnstake(value);
+            }
+          }}
+          isDisabled={_stakedBalance === 0 || value === 0}
+        >
+          Unstake
+        </Button>
+      </div>
+    </>
+  );
 };
